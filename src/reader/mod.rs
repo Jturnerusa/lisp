7ff9baf984e950cd2a3b783af433e6a91ad84fbd
John Turner <jturner.usa@gmail.com>
2024-04-08 19:01:10 -0400

    impl basic reader

---
+++ src/reader/mod.rs
@@ -0,0 +1,100 @@
+mod parse;
+
+use parse::Parser;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Value {
+    Cons(Box<Value>, Box<Value>),
+    Symbol(String),
+    String(String),
+    Int(u64),
+    Nil,
+}
+
+#[derive(Clone, Debug)]
+pub enum Error {
+    UnbalancedParens,
+    ParseError(String),
+}
+
+pub fn read(input: &str) -> Option<Result<Value, Error>> {
+    let mut parser = Parser::new(input);
+    match parser.next()? {
+        Ok(parse::Node::LeftParen) => Some(parse_cons(&mut parser)),
+        Ok(parse::Node::RightParen) => Some(Err(Error::UnbalancedParens)),
+        Ok(parse::Node::String(string)) => Some(Ok(Value::String(string.to_string()))),
+        Ok(parse::Node::Symbol(symbol)) => Some(Ok(Value::Symbol(symbol.to_string()))),
+        Ok(parse::Node::Int(i)) => Some(Ok(Value::Int(i.parse().unwrap()))),
+        Err(e) => Some(Err(Error::ParseError(e.to_string()))),
+    }
+}
+
+fn parse_cons<'a>(
+    nodes: &mut impl Iterator<Item = <Parser<'a> as Iterator>::Item>,
+) -> Result<Value, Error> {
+    Ok(match nodes.next() {
+        Some(Ok(parse::Node::LeftParen)) => {
+            Value::Cons(Box::new(parse_cons(nodes)?), Box::new(parse_cons(nodes)?))
+        }
+        Some(Ok(parse::Node::RightParen)) => Value::Nil,
+        Some(Ok(parse::Node::String(string))) => Value::Cons(
+            Box::new(Value::String(string.to_string())),
+            Box::new(parse_cons(nodes)?),
+        ),
+        Some(Ok(parse::Node::Symbol(symbol))) => Value::Cons(
+            Box::new(Value::Symbol(symbol.to_string())),
+            Box::new(parse_cons(nodes)?),
+        ),
+        Some(Ok(parse::Node::Int(i))) => Value::Cons(
+            Box::new(Value::Int(i.parse().unwrap())),
+            Box::new(parse_cons(nodes)?),
+        ),
+        None => return Err(Error::UnbalancedParens),
+        _ => todo!(),
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    macro_rules! boxed {
+        ($e:expr) => {
+            Box::new($e)
+        };
+    }
+
+    #[test]
+    fn test_simple() {
+        use Value::{Cons, Int, Nil, String, Symbol};
+        let expected = Cons(
+            boxed!(Symbol("hello".to_string())),
+            boxed!(Value::Cons(
+                boxed!(String("world".to_string())),
+                boxed!(Cons(boxed!(Int(1)), boxed!(Nil))),
+            )),
+        );
+        let value = read(r#"(hello "world" 1)"#).unwrap().unwrap();
+        assert_eq!(expected, value);
+    }
+
+    #[test]
+    fn test_nested() {
+        use Value::{Cons, Int, Nil, Symbol};
+        let a = Cons(
+            boxed!(Symbol("a".to_string())),
+            boxed!(Cons(boxed!(Int(1)), boxed!(Nil))),
+        );
+        let b = Cons(
+            boxed!(Symbol("b".to_string())),
+            boxed!(Cons(boxed!(Int(2)), boxed!(Nil))),
+        );
+        let bindings_list = Cons(boxed!(a), boxed!(Cons(boxed!(b), boxed!(Nil))));
+        let expected = Cons(
+            boxed!(Symbol("let".to_string())),
+            boxed!(Cons(boxed!(bindings_list), boxed!(Nil))),
+        );
+        let value = read(r#"(let ((a 1) (b 2)))"#).unwrap().unwrap();
+        assert_eq!(expected, value);
+    }
+}
---
+++ src/reader/parse/mod.rs
@@ -0,0 +1,112 @@
+use nom::branch;
+use nom::bytes::complete as bytes;
+use nom::combinator;
+use nom::sequence;
+
+type Result<'a, I = &'a str, O = I> = nom::IResult<I, O>;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Node<'a> {
+    LeftParen,
+    RightParen,
+    String(&'a str),
+    Symbol(&'a str),
+    Int(&'a str),
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Parser<'a> {
+    data: &'a str,
+}
+
+fn parse_node(input: &str) -> Result<&str, Node> {
+    sequence::delimited(
+        combinator::opt(whitespace),
+        branch::alt((left_paren, right_paren, string, symbol, int)),
+        combinator::opt(whitespace),
+    )(input)
+}
+
+fn left_paren(input: &str) -> Result<&str, Node> {
+    combinator::map(bytes::tag("("), |_| Node::LeftParen)(input)
+}
+
+fn right_paren(input: &str) -> Result<&str, Node> {
+    combinator::map(bytes::tag(")"), |_| Node::RightParen)(input)
+}
+
+fn string(input: &str) -> Result<&str, Node> {
+    combinator::map(
+        sequence::delimited(
+            bytes::tag(r#"""#),
+            bytes::is_not(r#"""#),
+            bytes::tag(r#"""#),
+        ),
+        Node::String,
+    )(input)
+}
+
+fn symbol(input: &str) -> Result<&str, Node> {
+    combinator::map(
+        combinator::recognize(sequence::preceded(
+            bytes::take_while_m_n(1, 1, |c: char| c.is_alphabetic()),
+            bytes::take_while(|c: char| c.is_alphanumeric()),
+        )),
+        Node::Symbol,
+    )(input)
+}
+
+fn int(input: &str) -> Result<&str, Node> {
+    combinator::map(bytes::take_while1(|c: char| c.is_ascii_digit()), Node::Int)(input)
+}
+
+fn whitespace(input: &str) -> Result {
+    bytes::take_while1(|c: char| c.is_whitespace())(input)
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { data: input }
+    }
+}
+
+impl<'a> Iterator for Parser<'a> {
+    type Item = std::result::Result<Node<'a>, &'a str>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.data.is_empty() {
+            match parse_node(self.data) {
+                Ok((rest, node)) => {
+                    self.data = rest;
+                    Some(Ok(node))
+                }
+                Err(nom::Err::Error(e) | nom::Err::Failure(e)) => Some(Err(e.input)),
+                _ => unreachable!(),
+            }
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_simple() {
+        const INPUT: &str = r#"(hello "world" 1)"#;
+        let mut parser = Parser::new(INPUT);
+        assert!(matches!(parser.next().unwrap().unwrap(), Node::LeftParen));
+        assert!(matches!(
+            parser.next().unwrap().unwrap(),
+            Node::Symbol("hello")
+        ));
+        assert!(matches!(
+            parser.next().unwrap().unwrap(),
+            Node::String("world")
+        ));
+        assert!(matches!(parser.next().unwrap().unwrap(), Node::Int("1")));
+        assert!(matches!(parser.next().unwrap().unwrap(), Node::RightParen));
+        assert!(parser.next().is_none());
+    }
+}
