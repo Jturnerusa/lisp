c53044b11d6519aba2236aaa70d55387713b5202
John Turner <jturner.usa@gmail.com>
2024-04-17 16:40:52 -0400

    impl Error type

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,28 @@
 pub mod reader;
 
 use std::collections::HashMap;
+use std::fmt;
 use std::rc::Rc;
 
 use unwrap_enum::{EnumAs, EnumIs};
 
 pub use reader::Reader;
+
+#[derive(Clone, Debug)]
+pub enum Error {
+    Type(Type, Type),
+    Parameters,
+    NotFound(String),
+}
+
+#[derive(Clone, Debug)]
+pub enum Type {
+    Function,
+    Cons,
+    Symbol,
+    String,
+    Int,
+}
 
 #[derive(Clone, Debug, EnumAs, EnumIs)]
 pub enum Object {
@@ -51,6 +68,30 @@ impl Object {
     }
 }
 
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Function => write!(f, "function"),
+            Self::Cons => write!(f, "cons"),
+            Self::Symbol => write!(f, "symbol"),
+            Self::String => write!(f, "string"),
+            Self::Int => write!(f, "integer"),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Type(expected, got) => write!(f, "expected type: {}: got :{}", expected, got),
+            Self::Parameters => write!(f, "invalid parameters"),
+            Self::NotFound(var) => write!(f, "variable not found: {}", var),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
 impl Iterator for Iter {
     type Item = (Rc<Object>, Rc<Object>);
     fn next(&mut self) -> Option<Self::Item> {
@@ -62,6 +103,18 @@ impl Iterator for Iter {
             Some((Rc::clone(car), Rc::clone(cdr)))
         } else {
             None
+        }
+    }
+}
+
+impl From<&Object> for Type {
+    fn from(other: &Object) -> Type {
+        match other {
+            Object::Function(..) => Type::Function,
+            Object::Cons(..) => Type::Cons,
+            Object::Symbol(_) => Type::Symbol,
+            Object::String(_) => Type::String,
+            Object::Int(_) => Type::Int,
         }
     }
 }
