0b9f5c2353826cf4cee337b08947ffa26429d571
John Turner <jturner.usa@gmail.com>
2024-04-17 17:32:27 -0400

    create Interpreter struct and impl lambda

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,66 @@ pub mod error;
 pub mod object;
 pub mod reader;
 
-pub use error::Error;
+use std::collections::HashMap;
+use std::rc::Rc;
+
+pub use error::{Error, Type};
 pub use object::Object;
 pub use reader::Reader;
+
+pub struct Interpreter {
+    globals: HashMap<String, Rc<Object>>,
+    locals: Vec<HashMap<String, Rc<Object>>>,
+}
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Self {
+            globals: HashMap::new(),
+            locals: Vec::new(),
+        }
+    }
+
+    pub fn eval(&mut self, object: Rc<Object>) -> Result<Rc<Object>, Error> {
+        match &*object {
+            Object::Cons(car, _) if matches!(&**car, Object::Symbol(s) if s.as_str() == "lambda") => {
+                self.lambda(object)
+            }
+            _ => todo!(),
+        }
+    }
+
+    fn lambda(&self, cons: Rc<Object>) -> Result<Rc<Object>, Error> {
+        let parameter_list = cons
+            .iter()
+            .ok_or(Error::Type(Type::Cons, Type::from(&*cons)))?
+            .map(|(car, _)| car)
+            .nth(1)
+            .ok_or(Error::Parameters)?;
+
+        let body = cons
+            .iter()
+            .ok_or(Error::Type(Type::Cons, Type::from(&*cons)))?
+            .map(|(car, _)| car)
+            .nth(2)
+            .ok_or(Error::Parameters)?;
+
+        let parameters: Vec<String> = parameter_list
+            .iter()
+            .ok_or(Error::Parameters)?
+            .map(|(car, _)| match &*car {
+                Object::Symbol(symbol) => Ok(symbol.clone()),
+                _ => Err(Error::Parameters),
+            })
+            .collect::<Result<Vec<_>, Error>>()?;
+
+        let captures = self
+            .locals
+            .iter()
+            .next_back()
+            .cloned()
+            .unwrap_or_else(HashMap::new);
+
+        Ok(Rc::new(Object::Function(body, parameters, captures)))
+    }
+}
---
+++ tests/lisp.rs
@@ -0,0 +1,24 @@
+use lisp::{Interpreter, Object, Reader};
+use std::rc::Rc;
+
+fn eval(expr: &str) -> Rc<Object> {
+    let mut interpreter = Interpreter::new();
+    let reader = Reader::new(expr);
+    let mut obj = None;
+    for read in reader {
+        obj = Some(
+            interpreter
+                .eval(Rc::new(Object::from(read.unwrap())))
+                .unwrap(),
+        );
+    }
+    obj.unwrap()
+}
+
+#[test]
+fn test_lambda() {
+    assert!(matches!(
+        *eval("(lambda (a b c) (+ a b c))"),
+        Object::Function(..)
+    ))
+}
