3f7b648af9b60388e49a9626030a7a0c85725419
John Turner <jturner.usa@gmail.com>
2024-04-16 21:11:10 -0400

    rm tests

--- tests/src/fac.lisp
+++
@@ -1,6 +0,0 @@
-(def fac (lambda (f)
-            (if (< f 2)
-                f
-                (* f (fac (- f 1))))))
-
-(print (fac 10))
--- tests/src/square.lisp
+++
@@ -1,8 +0,0 @@
-(def square (lambda (x)
-              (* x 2)))
-
-(def expected 4)
-
-(if (= (square 2) expected)
-    t
-    (panic "failure"))
