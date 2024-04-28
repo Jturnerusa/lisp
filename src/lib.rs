5b268b12ae5337bf02c8e3024eb73c903613504d
John Turner <jturner.usa@gmail.com>
2024-04-28 18:33:28 -0400

    impl Iterator for crate::Cons

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,47 @@ pub enum Value {
 
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Cons(Value, Value);
+
+pub struct Iter<'a>(Option<&'a Cons>);
+
+pub struct Cars<'a>(Iter<'a>);
+
+impl Cons {
+    pub fn iter(&self) -> Iter {
+        Iter(Some(self))
+    }
+
+    pub fn iter_cars(&self) -> Cars {
+        Cars(self.iter())
+    }
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a Cons;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(current) = self.0 {
+            self.0 = match &current.1 {
+                Value::Cons(cons) => Some(cons),
+                _ => None,
+            };
+            Some(current)
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> Iterator for Cars<'a> {
+    type Item = &'a Value;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|cons| &cons.0)
+    }
+}
+
+impl<'a> IntoIterator for &'a Cons {
+    type Item = &'a Cons;
+    type IntoIter = Iter<'a>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
