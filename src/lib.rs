e6382bd247e2515c46d8eb7eea99eea97e674025
John Turner <jturner.usa@gmail.com>
2024-04-28 18:24:32 -0400

    use Cons struct in Value::Cons rather than tuple-like enum variant

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,12 @@ use unwrap_enum::{EnumAs, EnumIs};
 // A unified lisp value representation.
 #[derive(Clone, PartialEq, Eq, Debug, EnumAs, EnumIs)]
 pub enum Value {
-    Cons(Box<Value>, Box<Value>),
+    Cons(Box<Cons>),
     Symbol(String),
     String(String),
     Int(i64),
     Nil,
 }
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct Cons(Value, Value);
