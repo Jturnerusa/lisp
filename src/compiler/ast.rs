5a8ad4c1fc48ff9f7e1af6d2c7a327ee77289b7f
John Turner <jturner.usa@gmail.com>
2024-05-15 17:53:49 -0400

    add more Ast enum variants

--- src/compiler/ast.rs
+++ src/compiler/ast.rs
@@ -6,6 +6,7 @@ use crate::{Cons, Value};
 pub enum Error {
     Lambda(&'static str),
     If(&'static str),
+    Parameters(&'static str),
 }
 
 #[derive(Clone, Debug, EnumAs, EnumIs)]
@@ -13,9 +14,18 @@ pub enum Ast {
     Lambda(Lambda),
     If(If),
     List(Vec<Ast>),
+    Add(Vec<Ast>),
+    Sub(Vec<Ast>),
+    Mul(Vec<Ast>),
+    Div(Vec<Ast>),
+    Car(Box<Ast>),
+    Cdr(Box<Ast>),
+    Cons(Box<Ast>, Box<Ast>),
     Symbol(String),
     String(String),
     Int(i64),
+    True,
+    Nil,
 }
 
 #[derive(Clone, Debug)]
@@ -51,15 +61,72 @@ pub fn parse(value: &Value) -> Result<Ast, Error> {
         {
             parse_if(cons)
         }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "+") => {
+            Ok(Ast::Add(
+                cons.iter_cars()
+                    .skip(1)
+                    .map(parse)
+                    .collect::<Result<Vec<_>, Error>>()?,
+            ))
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "-") => {
+            Ok(Ast::Sub(
+                cons.iter_cars()
+                    .skip(1)
+                    .map(parse)
+                    .collect::<Result<Vec<_>, Error>>()?,
+            ))
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "*") => {
+            Ok(Ast::Mul(
+                cons.iter_cars()
+                    .skip(1)
+                    .map(parse)
+                    .collect::<Result<Vec<_>, Error>>()?,
+            ))
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "/") => {
+            Ok(Ast::Div(
+                cons.iter_cars()
+                    .skip(1)
+                    .map(parse)
+                    .collect::<Result<Vec<_>, Error>>()?,
+            ))
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "car") => {
+            if cons.iter_cars().count() != 2 {
+                Err(Error::Parameters("car expects 1 parameter"))
+            } else {
+                Ok(Ast::Car(Box::new(parse(cons.iter_cars().nth(1).unwrap())?)))
+            }
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "cdr") => {
+            if cons.iter_cars().count() != 2 {
+                Err(Error::Parameters("cdr expects 1 parameter"))
+            } else {
+                Ok(Ast::Cdr(Box::new(parse(cons.iter_cars().nth(1).unwrap())?)))
+            }
+        }
+        Value::Cons(cons) if matches!(cons.0.as_symbol(), Some(sym) if sym.as_str() == "cons") => {
+            if cons.iter_cars().count() != 3 {
+                Err(Error::Parameters("cons expects 2 parameters"))
+            } else {
+                Ok(Ast::Cons(
+                    Box::new(parse(cons.iter_cars().nth(1).unwrap())?),
+                    Box::new(parse(cons.iter_cars().nth(2).unwrap())?),
+                ))
+            }
+        }
         Value::Cons(cons) => Ok(Ast::List(
             cons.iter_cars()
                 .map(parse)
                 .collect::<Result<Vec<_>, Error>>()?,
         )),
+        Value::Symbol(symbol) if symbol.as_str() == "t" => Ok(Ast::True),
         Value::Symbol(symbol) => Ok(Ast::Symbol(symbol.clone())),
         Value::String(string) => Ok(Ast::String(string.clone())),
         Value::Int(i) => Ok(Ast::Int(*i)),
-        _ => todo!(),
+        Value::Nil => Ok(Ast::Nil),
     }
 }
 
