11eabc12666c92d162b4d96fe7f4a341a9b8837f
John Turner <jturner.usa@gmail.com>
2024-05-16 23:00:17 -0400

    add interpreter tests

---
+++ tests/lisp.rs
@@ -0,0 +1,44 @@
+use std::convert::TryFrom;
+
+use compiler::Compiler;
+use reader::Reader;
+use value::Value;
+use vm::Vm;
+
+fn eval(input: &str) -> Result<Value, Box<dyn std::error::Error>> {
+    let reader = Reader::new(input);
+    let mut compiler = Compiler::new();
+    let mut vm = Vm::new();
+    let mut opcodes = Vec::new();
+
+    for read in reader {
+        let read = read.unwrap();
+        let ast = compiler::ast::parse(&read).unwrap();
+        opcodes.clear();
+        compiler.compile(&ast, &mut opcodes).unwrap();
+        dbg!(&opcodes);
+        vm.eval(opcodes.as_slice()).unwrap();
+    }
+
+    let ret = vm.stack().first().unwrap().borrow().clone();
+
+    Ok(Value::try_from(&ret).unwrap())
+}
+
+#[test]
+fn test_add() {
+    let input = "(+ 1 1)";
+    assert!(matches!(eval(input).unwrap(), Value::Int(2)));
+}
+
+#[test]
+fn test_nested_add() {
+    let input = "(+ (+ 1 1) 1)";
+    assert!(matches!(eval(input).unwrap(), Value::Int(3)));
+}
+
+#[test]
+fn test_div_by_zero() {
+    let input = "(+ 1 a)";
+    eval(input).unwrap();
+}
