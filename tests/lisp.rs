8a69cc6dcdb4f63c1fb3be9bb11acb16e86093df
John Turner <jturner.usa@gmail.com>
2024-05-18 19:53:17 -0400

    fixup tests to use Vm::eval return values

--- tests/lisp.rs
+++ tests/lisp.rs
@@ -1,37 +1,43 @@
-use std::convert::TryFrom;
+use std::cell::RefCell;
+use std::ops::Deref;
+use std::rc::Rc;
 
 use compiler::Compiler;
 use reader::Reader;
-use value::Value;
 use vm::Vm;
 
-fn eval(input: &str) -> Result<Value, Box<dyn std::error::Error>> {
+fn eval(input: &str) -> Result<Rc<RefCell<vm::Object>>, Box<dyn std::error::Error>> {
     let reader = Reader::new(input);
     let mut compiler = Compiler::new();
     let mut vm = Vm::new();
     let mut opcodes = Vec::new();
+    let mut ret = None;
 
     for read in reader {
         let read = read.unwrap();
         let ast = compiler::Ast::parse(&read).unwrap();
         opcodes.clear();
         compiler.compile(&ast, &mut opcodes).unwrap();
-        vm.eval(opcodes.as_slice()).unwrap();
+        ret = Some(vm.eval(opcodes.as_slice())?);
     }
 
-    let ret = vm.stack().first().unwrap().borrow().clone();
-
-    Ok(Value::try_from(&ret).unwrap())
+    Ok(ret.unwrap())
 }
 
 #[test]
 fn test_add() {
     let input = "(+ 1 1)";
-    assert!(matches!(eval(input).unwrap(), Value::Int(2)));
+    assert!(matches!(
+        eval(input).unwrap().borrow().deref(),
+        vm::Object::Int(2)
+    ))
 }
 
 #[test]
 fn test_nested_add() {
     let input = "(+ (+ 1 1) 1)";
-    assert!(matches!(eval(input).unwrap(), Value::Int(3)));
+    assert!(matches!(
+        eval(input).unwrap().borrow().deref(),
+        vm::Object::Int(3)
+    ))
 }
