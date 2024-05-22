e7f604285970ac8246679d47006476cb5529615b
John Turner <jturner.usa@gmail.com>
2024-05-22 15:28:53 -0400

    rm ast module

--- crates/compiler/src/lib.rs
+++ crates/compiler/src/lib.rs
@@ -1,34 +1,3 @@
 #![allow(dead_code)]
 
-pub mod ast;
 mod environment;
-
-use std::{collections::HashMap, ops::Deref};
-
-use thiserror::Error;
-
-use value::Value;
-use vm::{OpCode, Vm};
-
-pub use ast::Ast;
-use environment::{Environment, Variable};
-
-#[derive(Debug, Error)]
-pub enum Error {
-    #[error("vm error: {0}")]
-    Vm(#[from] vm::Error),
-}
-
-pub struct Compiler {
-    environment: Environment,
-    macros: HashMap<String, ast::Macro>,
-}
-
-impl Compiler {
-    pub fn new() -> Self {
-        Self {
-            environment: Environment::new(),
-            macros: HashMap::new(),
-        }
-    }
-}
