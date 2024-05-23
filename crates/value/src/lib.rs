5b1947a347381d8a0833cc159d5f6046537a8d77
John Turner <jturner.usa@gmail.com>
2024-05-23 19:00:57 -0400

    use Vec instead of linked lists to represent lists

--- crates/value/src/lib.rs
+++ crates/value/src/lib.rs
@@ -2,60 +2,12 @@
 
 use unwrap_enum::{EnumAs, EnumIs};
 
-// A unified lisp value representation.
 #[derive(Clone, PartialEq, Eq, Debug, EnumAs, EnumIs)]
 pub enum Value {
-    Cons(Box<Cons>),
+    List(Vec<Value>),
     Symbol(String),
     String(String),
     Int(i64),
     True,
     Nil,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub struct Cons(pub Value, pub Value);
-
-pub struct Iter<'a>(Option<&'a Cons>);
-
-pub struct Cars<'a>(Iter<'a>);
-
-impl Cons {
-    pub fn iter(&self) -> Iter {
-        Iter(Some(self))
-    }
-
-    pub fn iter_cars(&self) -> Cars {
-        Cars(self.iter())
-    }
-}
-
-impl<'a> Iterator for Iter<'a> {
-    type Item = &'a Cons;
-    fn next(&mut self) -> Option<Self::Item> {
-        if let Some(current) = self.0 {
-            self.0 = match &current.1 {
-                Value::Cons(cons) => Some(cons),
-                _ => None,
-            };
-            Some(current)
-        } else {
-            None
-        }
-    }
-}
-
-impl<'a> Iterator for Cars<'a> {
-    type Item = &'a Value;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next().map(|cons| &cons.0)
-    }
-}
-
-impl<'a> IntoIterator for &'a Cons {
-    type Item = &'a Cons;
-    type IntoIter = Iter<'a>;
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
 }
