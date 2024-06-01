5cd17c57e02a5d71e6be65b6aaa24746a690bc64
John Turner <jturner.usa@gmail.com>
2024-06-01 00:06:06 -0400

    lisp: deprecate defun style function definitions

--- lib/bootstrap.lisp
+++ lib/bootstrap.lisp
@@ -1,25 +1,28 @@
-(defmacro defun (name bindings)
-  (list 'def name (cons 'lambda (cons bindings &rest))))
-
 (defmacro let (bindings)
   (cons (cons 'lambda (cons (map car bindings) &rest))
         (map cadr bindings)))
 
 (eval-when-compile
-  (defun car (cons)
-    (car cons))
+  (def car (lambda (cons)
+             (car cons)))
 
-  (defun cadr (cons)
-    (car (cdr cons)))
+  (def cadr (lambda (cons)
+              (car (cdr cons))))
 
-  (defun map (fn list)
-    (if (nil? list)
-        nil
-        (cons (fn (car list))
-              (map fn (cdr list))))))
+  (def map (lambda (fn list)
+             (if (nil? list)
+                 nil
+                 (cons (fn (car list))
+                       (map fn (cdr list))))))
 
-(defun and (a b)
-  (if a (if b t nil) nil))
+  (def and (lambda (a b)
+             (if a
+                 (if b
+                     t nil)
+                 nil)))
 
-(defun or (a b)
-  (if a t (if b t nil)))
+  (def or (lambda (a b)
+            (if a
+                t
+                (if b
+                    t nil)))))
--- tests/lisp/fact.lisp
+++ tests/lisp/fact.lisp
@@ -1,6 +1,6 @@
-(defun fact (n)
-  (if (< n 2)
-      1
-      (* n (fact (- n 1)))))
+(def fact (lambda (n)
+            (if (< n 2)
+                1
+                (* n (fact (- n 1))))))
 
 (fact 10)
