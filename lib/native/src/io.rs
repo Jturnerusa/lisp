cc932ffb6a49438ed4cfe23ff03686a65f6f3e22
John Turner <jturner.usa@gmail.com>
2024-06-03 16:54:13 -0400

    lib/native: impl read-file

--- lib/native/src/io.rs
+++ lib/native/src/io.rs
@@ -1,4 +1,6 @@
-use vm::{Error, Object, Value};
+use std::{fs::File, io::Read, rc::Rc};
+
+use vm::{object::Type, Error, Object, Value};
 
 pub fn print(objects: &mut [Value]) -> Result<Object, Error> {
     if objects.len() > 1 {
@@ -8,5 +10,29 @@ pub fn print(objects: &mut [Value]) -> Result<Object, Error> {
     } else {
         objects.first().unwrap().with(|object| println!("{object}"));
         Ok(Object::Nil)
+    }
+}
+
+pub fn read_file(objects: &mut [Value]) -> Result<Object, Error> {
+    if objects.len() > 1 {
+        Err(Error::Parameters(
+            "read-file expects a single parameter".to_string(),
+        ))
+    } else {
+        let path = objects.first().unwrap().with(|object| match object {
+            Object::String(string) => Ok(string.clone()),
+            object => Err(Error::Type {
+                expected: Type::String,
+                recieved: Type::from(object),
+            }),
+        })?;
+
+        let mut file = File::open(path).map_err(|e| Error::Other(Rc::new(e)))?;
+        let mut buff = String::new();
+
+        file.read_to_string(&mut buff)
+            .map_err(|e| Error::Other(Rc::new(e)))?;
+
+        Ok(Object::String(buff))
     }
 }
--- lib/native/src/lib.rs
+++ lib/native/src/lib.rs
@@ -4,4 +4,5 @@ use vm::Vm;
 
 pub fn load_module(vm: &mut Vm) {
     vm.load_native_function("print", io::print);
+    vm.load_native_function("read-file", io::read_file);
 }
