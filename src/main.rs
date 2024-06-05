7e681354c454ecd42c35a4f65b5f9a26d90c76f3
John Turner <jturner.usa@gmail.com>
2024-06-05 17:57:03 -0400

    lisp: compile in one shot and then eval

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,41 @@ use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
-use vm::Vm;
+use vm::{Constant, OpCode, Vm};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut compiler = Compiler::new();
     let mut vm = Vm::new();
+    let mut opcodes = Vec::new();
+    let mut constants = HashMap::with_hasher(IdentityHasher::new());
 
     native_functions::load_module(&mut vm);
 
     for arg in env::args().skip(1) {
         let file = File::open(arg)?;
-        eval_file(file, &mut vm, &mut compiler)?;
+        compile_file(file, &mut compiler, &mut opcodes, &mut constants)?;
     }
+
+    vm.load_constants(constants.into_values());
+    vm.eval(opcodes.as_slice())?;
 
     Ok(())
 }
 
-fn eval_file(mut file: File, vm: &mut Vm, compiler: &mut Compiler) -> Result<(), Box<dyn Error>> {
+fn compile_file(
+    mut file: File,
+    compiler: &mut Compiler,
+    opcodes: &mut Vec<OpCode>,
+    constants: &mut HashMap<u64, Constant, IdentityHasher>,
+) -> Result<(), Box<dyn Error>> {
     let mut buff = String::new();
     file.read_to_string(&mut buff)?;
 
     let reader = Reader::new(buff.as_str());
-    let mut opcodes = Vec::new();
-    let mut constants = HashMap::with_hasher(IdentityHasher::new());
 
     for read in reader {
-        opcodes.clear();
-        constants.clear();
-
         let value = read?;
-
-        compiler.compile(&value, &mut opcodes, &mut constants)?;
-
-        vm.load_constants(constants.values().cloned());
-        vm.eval(opcodes.as_slice())?;
+        compiler.compile(&value, opcodes, constants)?;
     }
 
     Ok(())
