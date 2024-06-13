142a6bd84a15fbc1cfeef45a5534081e6a7232f4
John Turner <jturner.usa@gmail.com>
2024-06-13 16:17:27 -0400

    crates/gc: impl Trace and various other convenience traits

--- crates/gc/src/lib.rs
+++ crates/gc/src/lib.rs
@@ -1,6 +1,13 @@
-use std::cell::Cell;
+#![feature(vec_into_raw_parts)]
+
+use core::fmt;
+use std::alloc::Layout;
+use std::cell::{Cell, RefCell};
+use std::collections::HashMap;
+use std::hash::Hash;
 use std::marker::PhantomData;
-use std::ptr::NonNull;
+use std::ops::Deref;
+use std::ptr::{self, addr_of, NonNull};
 
 thread_local! {
     pub static HEAD: Cell<Option<NonNull<Inner<dyn Trace>>>> = Cell::new(None);
@@ -10,6 +17,8 @@ pub unsafe trait Trace {
     unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>));
 }
 
+#[repr(C)]
+#[derive(Debug)]
 pub struct Inner<T: Trace + ?Sized> {
     next: Cell<Option<NonNull<Inner<dyn Trace>>>>,
     prev: Cell<Option<NonNull<Inner<dyn Trace>>>>,
@@ -17,6 +26,7 @@ pub struct Inner<T: Trace + ?Sized> {
     data: T,
 }
 
+#[derive(Debug)]
 pub struct Gc<T: Trace + ?Sized> {
     inner: NonNull<Inner<T>>,
     phantom: PhantomData<T>,
@@ -101,8 +111,121 @@ impl<T: Trace + ?Sized> Drop for Gc<T> {
     }
 }
 
+impl<T: Trace + ?Sized> Deref for Gc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &self.inner.as_ref().data }
+    }
+}
+
+impl<T: Trace + PartialEq + ?Sized> PartialEq for Gc<T> {
+    fn eq(&self, other: &Self) -> bool {
+        unsafe {
+            let a = &self.inner.as_ref().data;
+            let b = &other.inner.as_ref().data;
+            a == b
+        }
+    }
+}
+
+impl<T: Trace + Eq + ?Sized> Eq for Gc<T> {}
+
+impl<T: Trace + PartialOrd + ?Sized> PartialOrd for Gc<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        unsafe {
+            let a = &self.inner.as_ref().data;
+            let b = &other.inner.as_ref().data;
+            a.partial_cmp(b)
+        }
+    }
+}
+
+impl<T: Trace + Ord + ?Sized> Ord for Gc<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        unsafe {
+            let a = &self.inner.as_ref().data;
+            let b = &other.inner.as_ref().data;
+            a.cmp(b)
+        }
+    }
+}
+
+impl<T: Trace + Hash + ?Sized> Hash for Gc<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        unsafe {
+            self.inner.as_ref().data.hash(state);
+        }
+    }
+}
+
+impl<T: Trace + fmt::Display> fmt::Display for Gc<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        unsafe { write!(f, "{}", self.inner.as_ref().data) }
+    }
+}
+
+impl<T: Trace> From<Vec<T>> for Gc<[T]> {
+    fn from(value: Vec<T>) -> Self {
+        unsafe {
+            let layout = Layout::new::<Inner<T>>()
+                .extend(Layout::array::<T>(value.len()).unwrap())
+                .unwrap();
+            let inner_ptr = std::alloc::alloc(layout.0);
+            let inner = ptr::slice_from_raw_parts(inner_ptr, value.len()) as *mut Inner<[T]>;
+
+            let (vec_ptr, len, cap) = value.into_raw_parts();
+
+            ptr::copy_nonoverlapping(vec_ptr, ptr::addr_of_mut!((*inner).data).cast(), len);
+
+            std::mem::drop(Vec::from_raw_parts(vec_ptr, 0, cap));
+
+            let nonnull = NonNull::new(inner).unwrap();
+
+            Self {
+                inner: nonnull,
+                phantom: PhantomData,
+            }
+        }
+    }
+}
+
 unsafe impl<T: Trace + 'static> Trace for Gc<T> {
     unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {
         tracer(self.inner)
+    }
+}
+
+unsafe impl<T: Trace> Trace for RefCell<T> {
+    unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {
+        self.borrow().deref().trace(tracer)
+    }
+}
+
+unsafe impl Trace for String {
+    unsafe fn trace(&self, _: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {}
+}
+
+unsafe impl<T: Trace> Trace for [T] {
+    unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {
+        for element in self {
+            element.trace(tracer)
+        }
+    }
+}
+
+unsafe impl<T: Trace> Trace for Vec<T> {
+    unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {
+        for element in self {
+            element.trace(tracer)
+        }
+    }
+}
+
+unsafe impl<K: Trace, V: Trace> Trace for HashMap<K, V> {
+    unsafe fn trace(&self, tracer: &mut dyn FnMut(NonNull<Inner<dyn Trace>>)) {
+        for (key, val) in self {
+            key.trace(tracer);
+            val.trace(tracer);
+        }
     }
 }
