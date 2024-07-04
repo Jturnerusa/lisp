d71efe4a87bb463d528c110f6e145c9ee4a8d052
John Turner <jturner.usa@gmail.com>
2024-07-03 20:50:17 -0400

    crates/reader: create Context struct and embed it in Sexpr

--- crates/reader/src/lib.rs
+++ crates/reader/src/lib.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 use logos::{Lexer, Logos};
+use std::{ops::Range, rc::Rc};
 use thiserror::Error;
 use unwrap_enum::{EnumAs, EnumIs};
 
@@ -63,40 +64,102 @@ enum Macro {
     Splice,
 }
 
-#[derive(Clone, Debug, PartialEq, PartialOrd, Hash, EnumAs, EnumIs)]
-pub enum Sexpr {
-    List(Vec<Sexpr>),
-    Symbol(String),
-    String(String),
-    Char(char),
-    Int(i64),
-    Bool(bool),
-    Nil,
+#[derive(Clone, Debug, EnumAs, EnumIs)]
+pub enum Sexpr<'a> {
+    List {
+        list: Vec<Sexpr<'a>>,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    Symbol {
+        symbol: String,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    String {
+        string: String,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    Char {
+        char: char,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    Int {
+        int: i64,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    Bool {
+        bool: bool,
+        context: &'a Context,
+        span: Range<usize>,
+    },
+    Nil {
+        context: &'a Context,
+        span: Range<usize>,
+    },
+}
+
+#[derive(Clone)]
+pub struct Context {
+    display: Rc<dyn fmt::Display>,
+    source: String,
 }
 
 pub struct Reader<'a> {
     lexer: Lexer<'a, Token>,
+    context: &'a Context,
+}
+
+impl Context {
+    pub fn new<D: fmt::Display + 'static>(source: &str, display: D) -> Self {
+        Self {
+            display: Rc::new(display),
+            source: source.to_string(),
+        }
+    }
+
+    pub fn source(&self) -> &str {
+        self.source.as_str()
+    }
+
+    pub fn display(&self) -> &dyn fmt::Display {
+        &self.display
+    }
+
+    pub fn span(&self, span: Range<usize>) -> &str {
+        &self.source[span.start..span.end]
+    }
 }
 
 impl<'a> Reader<'a> {
-    pub fn new(input: &'a str) -> Self {
+    pub fn new(context: &'a Context) -> Self {
         Self {
-            lexer: Lexer::new(input),
+            lexer: Lexer::new(context.source()),
+            context,
         }
     }
 }
 
-impl<'a> Iterator for Reader<'a> {
-    type Item = Result<Sexpr, Error<'a>>;
-    fn next(&mut self) -> Option<Self::Item> {
-        read(&mut self.lexer)
+impl fmt::Debug for Context {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Context: {}", self.display)
     }
 }
 
-impl fmt::Display for Sexpr {
+impl<'a> Iterator for Reader<'a> {
+    type Item = Result<Sexpr<'a>, Error<'a>>;
+    fn next(&mut self) -> Option<Self::Item> {
+        read(&mut self.lexer, &self.context)
+    }
+}
+
+impl<'a> fmt::Display for Sexpr<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Sexpr::List(list) => {
+            Sexpr::List { list, .. } => {
                 write!(f, "(")?;
                 for (i, sexpr) in list.iter().enumerate() {
                     write!(f, "{sexpr}")?;
@@ -107,89 +170,169 @@ impl fmt::Display for Sexpr {
                 write!(f, ")")?;
                 Ok(())
             }
-            Sexpr::Symbol(symbol) => write!(f, "{symbol}"),
-            Sexpr::String(string) => write!(f, r#""{string}""#),
-            Sexpr::Char(char) => write!(f, "'{char}'"),
-            Sexpr::Int(int) => write!(f, "{int}"),
-            Sexpr::Bool(bool) if !bool => write!(f, "false"),
-            Sexpr::Bool(_) => write!(f, "true"),
-            Self::Nil => write!(f, "()"),
+            Sexpr::Symbol { symbol, .. } => write!(f, "{symbol}"),
+            Sexpr::String { string, .. } => write!(f, r#""{string}""#),
+            Sexpr::Char { char, .. } => write!(f, "'{char}'"),
+            Sexpr::Int { int, .. } => write!(f, "{int}"),
+            Sexpr::Bool { bool, .. } if !bool => write!(f, "false"),
+            Sexpr::Bool { .. } => write!(f, "true"),
+            Self::Nil { .. } => write!(f, "()"),
         }
     }
 }
 
-fn read<'a>(lexer: &mut Lexer<'a, Token>) -> Option<Result<Sexpr, Error<'a>>> {
+fn read<'a>(
+    lexer: &mut Lexer<'a, Token>,
+    context: &'a Context,
+) -> Option<Result<Sexpr<'a>, Error<'a>>> {
     Some(Ok(match lexer.next()? {
-        Ok(Token::LeftParen) => match read_list(lexer) {
+        Ok(Token::LeftParen) => match read_list(lexer, context) {
             Ok(sexpr) => sexpr,
             Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
         },
         Ok(Token::RightParen) => return Some(Err(Error::UnbalancedParens)),
-        Ok(Token::Quote) => match expand_macro(lexer, Macro::Quote) {
+        Ok(Token::Quote) => match expand_macro(lexer, context, Macro::Quote) {
             Ok(sexpr) => sexpr,
             Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
         },
-        Ok(Token::QuasiQuote) => match expand_macro(lexer, Macro::QuasiQuote) {
+        Ok(Token::QuasiQuote) => match expand_macro(lexer, context, Macro::QuasiQuote) {
             Ok(sexpr) => sexpr,
             Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
         },
-        Ok(Token::UnQuote) => match expand_macro(lexer, Macro::UnQuote) {
+        Ok(Token::UnQuote) => match expand_macro(lexer, context, Macro::UnQuote) {
             Ok(sexpr) => sexpr,
             Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
         },
-        Ok(Token::Splice) => match expand_macro(lexer, Macro::Splice) {
+        Ok(Token::Splice) => match expand_macro(lexer, context, Macro::Splice) {
             Ok(sexpr) => sexpr,
             Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
         },
-        Ok(Token::Symbol) => Sexpr::Symbol(lexer.slice().to_string()),
-        Ok(Token::String) => Sexpr::String(lexer.slice().to_string()),
-        Ok(Token::Char) => Sexpr::Char(lexer.slice().parse().unwrap()),
-        Ok(Token::Int) => Sexpr::Int(lexer.slice().parse().unwrap()),
-        Ok(Token::True) => Sexpr::Bool(true),
-        Ok(Token::False) => Sexpr::Bool(false),
+        Ok(Token::Symbol) => Sexpr::Symbol {
+            symbol: lexer.slice().to_string(),
+            context,
+            span: lexer.span(),
+        },
+        Ok(Token::String) => Sexpr::String {
+            string: lexer.slice().to_string(),
+            context,
+            span: lexer.span(),
+        },
+        Ok(Token::Char) => Sexpr::Char {
+            char: lexer.slice().parse().unwrap(),
+            context,
+            span: lexer.span(),
+        },
+        Ok(Token::Int) => Sexpr::Int {
+            int: lexer.slice().parse().unwrap(),
+            context,
+            span: lexer.span(),
+        },
+        Ok(Token::True) => Sexpr::Bool {
+            bool: true,
+            context,
+            span: lexer.span(),
+        },
+        Ok(Token::False) => Sexpr::Bool {
+            bool: false,
+            context,
+            span: lexer.span(),
+        },
         Err(_) => return Some(Err(Error::Lexer(lexer.remainder()))),
     }))
 }
 
-fn read_list<'a>(lexer: &mut Lexer<'a, Token>) -> Result<Sexpr, Error<'a>> {
+fn read_list<'a>(
+    lexer: &mut Lexer<'a, Token>,
+    context: &'a Context,
+) -> Result<Sexpr<'a>, Error<'a>> {
     let mut list = Vec::new();
 
     loop {
         match lexer.next() {
-            Some(Ok(Token::LeftParen)) => list.push(read_list(lexer)?),
-            Some(Ok(Token::RightParen)) if list.is_empty() => return Ok(Sexpr::Nil),
-            Some(Ok(Token::RightParen)) => return Ok(Sexpr::List(list)),
-            Some(Ok(Token::Quote)) => list.push(expand_macro(lexer, Macro::Quote)?),
-            Some(Ok(Token::QuasiQuote)) => list.push(expand_macro(lexer, Macro::QuasiQuote)?),
-            Some(Ok(Token::UnQuote)) => list.push(expand_macro(lexer, Macro::UnQuote)?),
-            Some(Ok(Token::Splice)) => list.push(expand_macro(lexer, Macro::Splice)?),
-            Some(Ok(Token::Symbol)) => list.push(Sexpr::Symbol(lexer.slice().to_string())),
-            Some(Ok(Token::String)) => list.push(Sexpr::String(lexer.slice().to_string())),
-            Some(Ok(Token::Char)) => list.push(Sexpr::Char(lexer.slice().parse().unwrap())),
-            Some(Ok(Token::Int)) => list.push(Sexpr::Int(lexer.slice().parse().unwrap())),
-            Some(Ok(Token::True)) => list.push(Sexpr::Bool(true)),
-            Some(Ok(Token::False)) => list.push(Sexpr::Bool(false)),
+            Some(Ok(Token::LeftParen)) => list.push(read_list(lexer, context)?),
+            Some(Ok(Token::RightParen)) if list.is_empty() => {
+                return Ok(Sexpr::Nil {
+                    context,
+                    span: lexer.span(),
+                })
+            }
+            Some(Ok(Token::RightParen)) => {
+                return Ok(Sexpr::List {
+                    list,
+                    context,
+                    span: lexer.span(),
+                })
+            }
+            Some(Ok(Token::Quote)) => list.push(expand_macro(lexer, context, Macro::Quote)?),
+            Some(Ok(Token::QuasiQuote)) => {
+                list.push(expand_macro(lexer, context, Macro::QuasiQuote)?)
+            }
+            Some(Ok(Token::UnQuote)) => list.push(expand_macro(lexer, context, Macro::UnQuote)?),
+            Some(Ok(Token::Splice)) => list.push(expand_macro(lexer, context, Macro::Splice)?),
+            Some(Ok(Token::Symbol)) => list.push(Sexpr::Symbol {
+                symbol: lexer.slice().to_string(),
+                context,
+                span: lexer.span(),
+            }),
+            Some(Ok(Token::String)) => list.push(Sexpr::String {
+                string: lexer.slice().to_string(),
+                context,
+                span: lexer.span(),
+            }),
+            Some(Ok(Token::Char)) => list.push(Sexpr::Char {
+                char: lexer.slice().parse().unwrap(),
+                context,
+                span: lexer.span(),
+            }),
+            Some(Ok(Token::Int)) => list.push(Sexpr::Int {
+                int: lexer.slice().parse().unwrap(),
+                context,
+                span: lexer.span(),
+            }),
+            Some(Ok(Token::True)) => list.push(Sexpr::Bool {
+                bool: true,
+                context,
+                span: lexer.span(),
+            }),
+            Some(Ok(Token::False)) => list.push(Sexpr::Bool {
+                bool: false,
+                context,
+                span: lexer.span(),
+            }),
             Some(Err(_)) => return Err(Error::Lexer(lexer.remainder())),
             None => return Err(Error::UnExpectedEof),
         }
     }
 }
 
-fn expand_macro<'a>(lexer: &mut Lexer<'a, Token>, r#macro: Macro) -> Result<Sexpr, Error<'a>> {
-    Ok(Sexpr::List(vec![
-        Sexpr::Symbol(
-            match r#macro {
-                Macro::Quote => "quote",
-                Macro::QuasiQuote => "quasiquote",
-                Macro::UnQuote => "unquote",
-                Macro::Splice => "splice",
-            }
-            .to_string(),
-        ),
-        match read(lexer) {
-            Some(Ok(sexpr)) => sexpr,
-            Some(Err(_)) => return Err(Error::Lexer(lexer.remainder())),
-            None => return Err(Error::UnExpectedEof),
-        },
-    ]))
+fn expand_macro<'a>(
+    lexer: &mut Lexer<'a, Token>,
+    context: &'a Context,
+    r#macro: Macro,
+) -> Result<Sexpr<'a>, Error<'a>> {
+    let span = lexer.span();
+
+    let symbol = Sexpr::Symbol {
+        symbol: match r#macro {
+            Macro::Quote => "quote",
+            Macro::QuasiQuote => "quasiquote",
+            Macro::UnQuote => "unquote",
+            Macro::Splice => "splice",
+        }
+        .to_string(),
+        context,
+        span: span.clone(),
+    };
+
+    let body = match read(lexer, context) {
+        Some(Ok(sexpr)) => sexpr,
+        Some(Err(_)) => return Err(Error::Lexer(lexer.remainder())),
+        None => return Err(Error::UnExpectedEof),
+    };
+
+    Ok(Sexpr::List {
+        list: vec![symbol, body],
+        context,
+        span,
+    })
 }
