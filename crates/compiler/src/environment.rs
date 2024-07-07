efb3de61b6d8ccc2a3c8bc78f5ebf3ad4784e97e
John Turner <jturner.usa@gmail.com>
2024-07-06 23:00:09 -0400

    crates/compiler: add type support to Environment

--- crates/compiler/src/environment.rs
+++ crates/compiler/src/environment.rs
@@ -1,49 +1,44 @@
+use crate::Type;
+use std::collections::HashMap;
 use vm::UpValue;
 
-use unwrap_enum::EnumIs;
-
-#[derive(Debug, Clone, Copy, EnumIs)]
-pub(super) enum Variable {
-    Local(usize),
-    Upvalue(usize),
-    Global,
+#[derive(Clone, Debug)]
+pub(crate) enum Variable {
+    Local(usize, Option<Type>),
+    Upvalue(usize, Option<Type>),
+    Global(Option<Type>),
 }
 
+#[derive(Clone, Debug)]
 struct Scope {
-    locals: Vec<String>,
+    locals: Vec<(String, Option<Type>)>,
     upvalues: Vec<(String, UpValue)>,
 }
 
-pub struct Environment {
+#[derive(Clone, Debug)]
+pub(crate) struct Environment {
+    globals: HashMap<String, Option<Type>>,
     scopes: Vec<Scope>,
 }
 
 impl Scope {
-    pub fn new() -> Self {
-        Self {
-            locals: Vec::new(),
-            upvalues: Vec::new(),
-        }
-    }
-}
-
-impl Scope {
-    fn get_local(&self, var: &str) -> Option<usize> {
-        self.locals.iter().enumerate().find_map(
-            |(i, local)| {
-                if local == var {
-                    Some(i)
+    fn get_local(&self, name: &str) -> Option<(usize, Option<Type>)> {
+        self.locals
+            .iter()
+            .enumerate()
+            .find_map(|(i, (local, r#type))| {
+                if name == local {
+                    Some((i, r#type.clone()))
                 } else {
                     None
                 }
-            },
-        )
+            })
     }
 
-    fn get_upvalue(&self, var: &str) -> Option<usize> {
+    fn get_upvalue(&self, name: &str) -> Option<usize> {
         self.upvalues.iter().enumerate().find_map(
             |(i, (upvalue, _))| {
-                if upvalue == var {
+                if name == upvalue {
                     Some(i)
                 } else {
                     None
@@ -54,44 +49,58 @@ impl Scope {
 }
 
 impl Environment {
-    pub fn new() -> Self {
-        Self { scopes: Vec::new() }
+    pub(crate) fn new() -> Self {
+        Self {
+            globals: HashMap::new(),
+            scopes: Vec::new(),
+        }
+    }
+
+    pub(crate) fn insert_global(&mut self, name: &str, r#type: Option<Type>) {
+        self.globals.insert(name.to_string(), r#type);
+    }
+
+    pub(crate) fn push_scope(&mut self, locals: impl Iterator<Item = (String, Option<Type>)>) {
+        self.scopes.push(Scope {
+            locals: locals.collect(),
+            upvalues: Vec::new(),
+        });
+    }
+
+    pub(crate) fn pop_scope(&mut self) {
+        self.scopes.pop().unwrap();
     }
 
     #[allow(clippy::manual_map)]
-    pub fn resolve(&mut self, var: &str) -> Variable {
-        let Some(current_scope) = self.scopes.last() else {
-            return Variable::Global;
-        };
-        if let Some(i) = current_scope.get_local(var) {
-            Variable::Local(i)
-        } else if let Some(i) = current_scope.get_upvalue(var) {
-            Variable::Upvalue(i)
-        } else if let Some(upvalue) = search_for_upvalue(var, self.scopes.iter_mut().rev().skip(1))
-        {
+    pub(crate) fn resolve(&mut self, name: &str) -> Option<Variable> {
+        if let Some((i, r#type)) = self.scopes.last().unwrap().get_local(name) {
+            Some(Variable::Local(i, r#type))
+        } else if let Some(i) = self.scopes.last().unwrap().get_upvalue(name) {
+            let r#type = search_upvalue_type(
+                self.scopes.last().unwrap().upvalues[i].1,
+                self.scopes.iter(),
+            );
+
+            Some(Variable::Upvalue(i, r#type))
+        } else if let Some(upvalue) = search_for_upvalue(name, self.scopes.iter_mut()) {
             self.scopes
                 .last_mut()
                 .unwrap()
                 .upvalues
-                .push((var.to_string(), upvalue));
-            let i = self.scopes.last().unwrap().get_upvalue(var).unwrap();
-            Variable::Upvalue(i)
+                .push((name.to_string(), upvalue));
+
+            let r#type = search_upvalue_type(upvalue, self.scopes.iter());
+            let i = self.scopes.last().unwrap().get_upvalue(name).unwrap();
+
+            Some(Variable::Upvalue(i, r#type))
+        } else if let Some(r#type) = self.globals.get(name) {
+            Some(Variable::Global(r#type.clone()))
         } else {
-            Variable::Global
+            None
         }
     }
 
-    pub fn push_scope(&mut self, locals: impl Iterator<Item = String>) {
-        let mut scope = Scope::new();
-        scope.locals = locals.collect();
-        self.scopes.push(scope);
-    }
-
-    pub fn pop_scope(&mut self) {
-        self.scopes.pop().unwrap();
-    }
-
-    pub fn upvalues(&self) -> impl Iterator<Item = UpValue> + '_ {
+    pub(crate) fn upvalues(&self) -> impl Iterator<Item = UpValue> + '_ {
         self.scopes
             .last()
             .unwrap()
@@ -99,36 +108,29 @@ impl Environment {
             .iter()
             .map(|(_, upvalue)| *upvalue)
     }
-
-    pub fn is_global_scope(&self) -> bool {
-        self.scopes.is_empty()
-    }
 }
 
 fn search_for_upvalue<'a>(
-    var: &str,
+    name: &str,
     mut scopes: impl Iterator<Item = &'a mut Scope>,
 ) -> Option<UpValue> {
     let scope = scopes.next()?;
-    if let Some(i) = scope.get_local(var) {
-        Some(UpValue::Local(i))
+    if let Some((local, _)) = scope.get_local(name) {
+        Some(UpValue::Local(local))
     } else {
-        let upvalue = search_for_upvalue(var, scopes)?;
-        scope.upvalues.push((var.to_string(), upvalue));
+        let upvalue = search_for_upvalue(name, scopes)?;
+        scope.upvalues.push((name.to_string(), upvalue));
         Some(UpValue::UpValue(scope.upvalues.len() - 1))
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_upvalues() {
-        let mut env = Environment::new();
-
-        env.push_scope(["a", "b", "c"].into_iter().map(str::to_string));
-        env.push_scope(["d", "e", "f"].into_iter().map(str::to_string));
-        env.push_scope(["g", "h", "i"].into_iter().map(str::to_string));
+fn search_upvalue_type<'a>(
+    upvalue: UpValue,
+    mut scopes: impl Iterator<Item = &'a Scope>,
+) -> Option<Type> {
+    let scope = scopes.next().unwrap();
+    match upvalue {
+        UpValue::Local(i) => scope.locals[i].1.clone(),
+        UpValue::UpValue(i) => search_upvalue_type(scope.upvalues[i].1, scopes),
     }
 }
--- crates/compiler/src/lib.rs
+++ crates/compiler/src/lib.rs
@@ -1,5 +1,21 @@
 #![allow(dead_code)]
 #![feature(let_chains, impl_trait_in_assoc_type)]
 
+use std::collections::HashSet;
+
 mod ast;
 mod environment;
+
+#[derive(Clone, Debug)]
+pub(crate) enum Type {
+    List(Box<Type>),
+    Cons,
+    Function,
+    Symbol,
+    String,
+    Char,
+    Int,
+    Bool,
+    Nil,
+    Union(HashSet<Type>),
+}
