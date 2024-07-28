2823810613ab13824b0315783913ca93727fd6b5
John Turner <jturner.usa@gmail.com>
2024-07-27 23:28:01 -0400

    lib/native: add more decls

--- lib/native/decl/native.lisp
+++ lib/native/decl/native.lisp
@@ -4,6 +4,23 @@
 
 (decl read-file (lambda (string)))
 
+(decl is-digit? (lambda ((s string)) -> bool))
+
 (decl string-split-whitespace (lambda (string)))
 
 (decl string->int (lambda (string)))
+
+(decl string-contains? (lambda ((haystack string)
+                                (needle (union string char)))
+                         -> string))
+
+(decl string-substr (lambda ((s string)
+                             (start int)
+                             (stop int))
+                      -> string))
+
+(decl string-find (lambda ((haystack string)
+                           (needle string))
+                    -> (union int nil)))
+
+(decl string->list (lambda ((s string)) -> (list char)))
