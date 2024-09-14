e69fa00989b9814677a254effec5e69564750cb1
John Turner <jturner.usa@gmail.com>
2024-09-14 19:00:25 -0400

    lisp: add annotations to upvalues test

--- tests/lisp/upvalues.lisp
+++ tests/lisp/upvalues.lisp
@@ -1,6 +1,7 @@
-(def upvalues (lambda (test)
-                ((lambda ()
-                   ((lambda ()
-                      test))))))
+(def (upvalues (fn int -> int))
+    (lambda (test)
+      ((lambda ()
+         ((lambda ()
+            test))))))
 
 (= (upvalues 1) 1)
