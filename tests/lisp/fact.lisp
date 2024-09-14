a7acb7d550ba897033e722fbe8e321ca4cf8312b
John Turner <jturner.usa@gmail.com>
2024-09-14 19:57:17 -0400

    lisp: add type annotations to test_fact

--- tests/lisp/fact.lisp
+++ tests/lisp/fact.lisp
@@ -1,6 +1,7 @@
-(def fact (lambda (n)
-            (if (< n 2)
-                1
-                (* n (fact (- n 1))))))
+(def (fact (fn int -> int))
+    (lambda (n)
+      (if (< n 2)
+          1
+          (* n (fact (- n 1))))))
 
 (fact 10)
