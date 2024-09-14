923c4f2fa869f526ca20475252ce02be004ae904
John Turner <jturner.usa@gmail.com>
2024-09-14 18:57:05 -0400

    lisp: add type annotations to named-let test

--- tests/lisp/named-let.lisp
+++ tests/lisp/named-let.lisp
@@ -1,7 +1,8 @@
-(def named-let-test (lambda (n)
-                      (named-let loop ((counter n))
-                        (if (> counter 0)
-                            (loop (- counter 1))
-                            counter))))
+(def (named-let-test (fn int -> int))
+    (lambda (n)
+      (named-let loop ((counter n))
+        (if (> counter 0)
+            (loop (- counter 1))
+            counter))))
 
 (assert (= (named-let-test 10) 0))
